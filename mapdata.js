0bc18c417092faaa10746ff4cf8521f0df983f8f
purvigujar <purvigujar1@gmail.com>
2023-04-16 02:20:18 +0530

    embeded map

---
+++ mapdata.js
@@ -0,0 +1,233 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    state_description: "",
+    state_color: "#88A4BC",
+    state_hover_color: "black",
+    state_url: "https://simplemaps.com",
+    border_size: "2",
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      color: "pink"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      color: "red"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      color: "yellow"
+    },
+    "4": {
+      name: "Assam",
+      color: "blue"
+    },
+    "5": {
+      name: "Bihar",
+      color: "green"
+    },
+    "6": {
+      name: "Chandigarh",
+      color: "red"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      color: "yellow"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      color: "blue"
+    },
+    "9": {
+      name: "Daman And Diu",
+      color: "purple"
+    },
+    "10": {
+      name: "Delhi",
+      color: "pink"
+    },
+    "11": {
+      name: "Goa",
+      color: "green"
+    },
+    "12": {
+      name: "Gujarat",
+      color: "blue"
+    },
+    "13": {
+      name: "Haryana",
+      color: "pink"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      color: "blue"
+    },
+    "16": {
+      name: "Jharkhand",
+      color: "pink"
+    },
+    "17": {
+      name: "Karnataka",
+      color: "blue"
+    },
+    "18": {
+      name: "Kerala",
+      color: "yellow"
+    },
+    "19": {
+      name: "Lakshadweep",
+      color: "light green"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      color: "red"
+    },
+    "21": {
+      name: "Maharashtra",
+      color: "green"
+    },
+    "22": {
+      name: "Manipur",
+      color: "red"
+    },
+    "23": {
+      name: "Meghalaya",
+      color: "green"
+    },
+    "24": {
+      name: "Mizoram",
+      color: "light blue"
+    },
+    "25": {
+      name: "Nagaland",
+      color: "pink"
+    },
+    "26": {
+      name: "Orissa",
+      color: "purple"
+    },
+    "27": {
+      name: "Puducherry",
+      color: "yellow"
+    },
+    "28": {
+      name: "Punjab",
+      color: "green"
+    },
+    "29": {
+      name: "Rajasthan",
+      color: "orange"
+    },
+    "30": {
+      name: "Sikkim",
+      color: "green"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      color: "green"
+    },
+    "32": {
+      name: "Tripura",
+      color: "pink"
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      color: "light blue"
+    },
+    "34": {
+      name: "Uttaranchal",
+      color: "red"
+    },
+    "35": {
+      name: "West Bengal",
+      color: "blue"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      color: "yellow"
+    },
+    "37": {
+      name: "Telangana",
+      color: "pink"
+    },
+    "38": {
+      name: "Ladakh",
+      color: "orange"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 18.987807,
+      lng: 72.836447,
+      name: "Mumbai"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
