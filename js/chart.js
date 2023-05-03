1bc0a13615d44db84c31d7d2e6c7ee18a3236ae0
purvigujar <purvigujar1@gmail.com>
2023-05-03 15:42:40 +0530

    crop page

---
+++ js/chart.js
@@ -0,0 +1,100 @@
+
+// chart colors
+var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+
+/* large line chart */
+var chLine = document.getElementById("chLine");
+var chartData = {
+  labels: ["May", "June", "July", "Aug", "Sept", "Oct", "Nov"],
+  datasets: [{
+    data: [589, 445, 483, 503, 689, 692, 634],
+    backgroundColor: 'transparent',
+    borderColor: colors[0],
+    borderWidth: 4,
+    pointBackgroundColor: colors[0]
+  }
+//   {
+//     data: [639, 465, 493, 478, 589, 632, 674],
+//     backgroundColor: colors[3],
+//     borderColor: colors[1],
+//     borderWidth: 4,
+//     pointBackgroundColor: colors[1]
+//   }
+  ]
+};
+if (chLine) {
+  new Chart(chLine, {
+  type: 'line',
+  data: chartData,
+  options: {
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    },
+    legend: {
+      display: false
+    },
+    responsive: true
+  }
+  });
+}
+
+
+var chLine1 = document.getElementById("chLine1");
+if (chLine1) {
+  new Chart(chLine1, {
+      type: 'line',
+      data: {
+          labels: ['Jan','Feb','Mar','Apr','May'],
+          datasets: [
+            {
+              backgroundColor:'#ffffff',
+              borderColor:'#ffffff',
+              data: [10, 11, 4, 11, 4],
+              fill: false
+            }
+          ]
+      },
+      options: lineOptions
+  });
+}
+var chLine2 = document.getElementById("chLine2");
+if (chLine2) {
+  new Chart(chLine2, {
+      type: 'line',
+      data: {
+          labels: ['A','B','C','D','E'],
+          datasets: [
+            {
+              backgroundColor:'#ffffff',
+              borderColor:'#ffffff',
+              data: [4, 5, 7, 13, 12],
+              fill: false
+            }
+          ]
+      },
+      options: lineOptions
+  });
+}
+
+var chLine3 = document.getElementById("chLine3");
+if (chLine3) {
+  new Chart(chLine3, {
+      type: 'line',
+      data: {
+          labels: ['Pos','Neg','Nue','Other','Unknown'],
+          datasets: [
+            {
+              backgroundColor:'#ffffff',
+              borderColor:'#ffffff',
+              data: [13, 15, 10, 9, 14],
+              fill: false
+            }
+          ]
+      },
+      options: lineOptions
+  });
+}
